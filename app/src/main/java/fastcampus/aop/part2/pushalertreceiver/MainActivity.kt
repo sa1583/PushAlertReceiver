67cca421ef196635df7f4fbef5f213c8c8ed7b07
sa1583 <sa1583@naver.com>
2022-03-08 16:38:44 +0900

    basic ui

--- app/src/main/java/fastcampus/aop/part2/pushalertreceiver/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/pushalertreceiver/MainActivity.kt
@@ -1,9 +1,18 @@
 package fastcampus.aop.part2.pushalertreceiver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private val firebaseToken: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
