8d98c60f3e4935e198f3c57443904dd9de38c940
sa1583 <sa1583@naver.com>
2022-03-09 18:02:54 +0900

    add core function

--- app/src/main/java/fastcampus/aop/part2/pushalertreceiver/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/pushalertreceiver/MainActivity.kt
@@ -1,5 +1,6 @@
 package fastcampus.aop.part2.pushalertreceiver
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +21,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initFirebase()
+        updateResult()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        updateResult(true)
     }
 
     private fun initFirebase() {
@@ -30,5 +39,14 @@ class MainActivity : AppCompatActivity() {
                     firebaseToken.text = token
                 }
             }
+    }
+
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+        if (isNewIntent) {
+            "(으)로 갱신했습니다."
+        } else {
+            "(으)로 실행했습니다."
+        }
     }
 }
